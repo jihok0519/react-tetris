9b300cbeb4edacd26f2e985a9bc30701c2520a7f
Enoch Kim <jihok0519@gmail.com>
2022-09-16 17:41:54 -0400

    added logic to check for collision on game parameters

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -7,3 +7,22 @@ export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
     )
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY }) => {
+    for (let y = 0; y < player.tetromino.length; y += 1) {
+        for (let x = 0; x < player.tetromino[y].length; x += 1) {
+            // 1. check we're on a tetromino cell.
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                // 2. check that our moves are inside the game area's parameters(width & height).
+                !stage[y + player.pos.y + moveY] ||
+                !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                // 3. check that the cell we're moving to isn't clear.
+                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
+                ) {
+                return true;
+                }
+            }
+        }
+    }
+};
